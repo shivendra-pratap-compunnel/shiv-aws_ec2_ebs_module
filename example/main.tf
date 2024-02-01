509489499cdc64126afb626f16e054a315899291
Shivendra <157563011+shivendra-pratap-compunnel@users.noreply.github.com>
2024-02-01 14:52:39 +0530

    Add files via upload

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,9 @@
 module "ec2" {
-  source                 = "git::https://github.com/shivendra-pratap-compunnel/aws_ec2_ebs_module.git"
+  source                 = "git::https://github.com/CD-TeraformModules/aws-ec2-with-ebs.git"
   ami                    = "ami-0c7217cdde317cfec"
   instance_name          = "example_ec2"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-0dbfd494bed5e28dd"]
+  vpc_security_group_ids = ["sg-0dbffg94bed5e28dd"]
   ebs_size               = "9"
   ebs_type               = "gp3"
 }
